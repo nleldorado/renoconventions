443861f98d166db377a09ab3daf6b13f988864d5
nleldorado <nle@eldoradoresorts.com>
2020-03-23 14:01:08 -0700

    edit counter

--- src/components/CountIntersectionObserver.js
+++ src/components/CountIntersectionObserver.js
@@ -5,16 +5,16 @@ const CountIntersectionObserver = () => {
     return (
         <div className="countdown">
                 <div className="count">
-                <Counter target={3} title='Resorts' duration={2} />
+                <Counter target={3} title='Resorts' duration={3} />
                 </div>
                 <div className="count">
-                <Counter target={5000} title='Luxury Rooms/Suites' duration={2} />
+                <Counter target={5000} title='Luxury Rooms/Suites' duration={3} />
                 </div>
                 <div className="count">
-                <Counter target={55} title='Restaurants' duration={2} />
+                <Counter target={55} title='Restaurants' duration={3} />
                 </div>
                 <div className="count">
-                <Counter target={100} title='Meeting Space' duration={2} />
+                <Counter target={100} title='Meeting Space' duration={3} />
                 </div>
      
         </div>
