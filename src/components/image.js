4f08cfb2f2cc01b6158267bfcc8d9ae514f6efe6
nleldorado <nle@eldoradoresorts.com>
2020-04-01 17:12:21 -0700

    update slider

--- src/components/image.js
+++ src/components/image.js
@@ -8,7 +8,7 @@ const Image = () => {
       placeholderImage: file(relativePath: { eq: "row-white.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+            ...GatsbyImageSharpFluid_tracedSVG
           }
         }
       }
