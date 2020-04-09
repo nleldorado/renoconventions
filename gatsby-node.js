f7e0c155deb8dd5da01c2f8e777e78cfd261c76a
nleldorado <nle@eldoradoresorts.com>
2020-04-09 15:18:38 -0700

    make changes

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,3 +36,18 @@ exports.createPages = async ({ actions, graphql}) => {
     )
 
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /bad-module/,
+              use: loaders.null(),
+            },
+          ],
+        },
+      })
+    }
+  }
--- src/pages/team.js
+++ src/pages/team.js
@@ -4,6 +4,7 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import TeamCard from '../components/TeamCard'
 import TeamParallax from '../components/TeamParallax'
+import { graphql } from 'gatsby'
 
 const Team = ({ data }) => {
     const team = data.allNodeTeamMember.nodes;
